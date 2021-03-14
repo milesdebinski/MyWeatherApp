b38073e8af266ea6656b7fb9c67c1bd08e5b6d76
Miles Debinski <miles.debinski@gmail.com>
2021-03-14 14:40:29 +0000

    add detailed api fetching

--- script.js
+++ script.js
@@ -23,6 +23,35 @@ function getResult(query) {
 }
 
 
-function displayResult(master) {
-  console.log(master)
+function displayResult(wAPI) {
+  console.log(wAPI)
+  let city = document.querySelector('.city');
+  city.innerText = `${wAPI.name}, ${wAPI.sys.country}`
+
+  let now = new Date();
+  let date = document.querySelector('.date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.temp');
+  temp.innerText = `${Math.round(wAPI.main.temp)}°c`
+
+  let sky = document.querySelector('.sky');
+  sky.innerText = wAPI.weather[0].main
+
+  let minmax = document.querySelector('.min-max');
+  minmax.innerText = `${Math.round(wAPI.main.temp_min)}°c ~ ${Math.round(wAPI.main.temp_max)}°c`
+
+  function dateBuilder (d) {
+   let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   let months = ['January', 'February', 'March','April', 'May', 'January', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return ` ${day} ${date} ${month} ${year}` 
+  }
+
+
+
 }
